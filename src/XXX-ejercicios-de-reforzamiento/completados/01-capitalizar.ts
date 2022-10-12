9df1c5b9a95214f391659a6e291af827bc29bff5
Cartago <cartago.relaxingcup@gmail.com>
2022-10-12 14:42:24 +0200

    organizamos los ejerciccios en completados y el enunciado, y terminamos hasta el ejercicio 5

--- src/XXX-ejercicios-de-reforzamiento/completados/01-capitalizar.ts
+++ src/XXX-ejercicios-de-reforzamiento/completados/01-capitalizar.ts
@@ -4,35 +4,34 @@
  * Nota: NO hay que usar el ciclo "FOR OF", usar un observable y llamar la función capitalizar
  */
 
- import { from, map } from "rxjs";
+/**
+ * Salida esperada:
+ * Batman
+ * Joker
+ * Doble Cara
+ * Pingüino
+ * Hiedra Venenosa
+ */
+import { from, map } from "rxjs";
 
- /**
-  * Salida esperada:
-  * Batman
-  * Joker
-  * Doble Cara
-  * Pingüino
-  * Hiedra Venenosa
-  */
- const nombres = [
-     "batman",
-     "joker",
-     "doble cara",
-     "pingüino",
-     "hiedra venenosa",
- ];
- 
- const capitalizar = (nombre: string) =>
-     nombre.replace(
-         /\w\S*/g,
-         (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-     );
- 
- // Cambiar este FOR OF, por un observable y capitalizar las emisiones
- //- for( let nombre of nombres ) {
- //   console.log( capitalizar(nombre) )
- // }
- from(nombres)
-     .pipe(map((elem) => capitalizar(elem)))
-     .subscribe(console.log);
- 
+(() => {
+	const nombres = [
+		"batman",
+		"joker",
+		"doble cara",
+		"pingüino",
+		"hiedra venenosa",
+	];
+
+	const capitalizar = (nombre: string) =>
+		nombre.replace(
+			/\w\S*/g,
+			(txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+		);
+
+	// Cambiar este FOR OF, por un observable y capitalizar las emisiones
+	//- for( let nombre of nombres ) {
+	//   console.log( capitalizar(nombre) )
+	// }
+	from(nombres).pipe(map(capitalizar)).subscribe(console.log);
+})();
--- src/XXX-ejercicios-de-reforzamiento/completados/02-reduce.ts
+++ src/XXX-ejercicios-de-reforzamiento/completados/02-reduce.ts
@@ -1,3 +1,24 @@
+import { filter, from } from "rxjs";
+import { reduce } from "rxjs/operators";
 
+/**
+ * Ejercicio:
+ * Sume todos los números del arreglo usando un reduce.
+ * Debe de filtrar para que sólo números sean procesados
+ * La salida debe de ser 32
+ *
+ * Tip:
+ * isNan() es una función de JavaScript para determinar si es número
+ * Usar filter<any>(...) para no tener problemas de tipado.
+ */
 
-		
+(() => {
+	const datos = [1, 2, "foo", 3, 5, 6, "bar", 7, 8];
+	from(datos)
+		.pipe(
+			// Trabajar aquí
+			filter<any>((elem) => !isNaN(elem)),
+			reduce((acc, num) => acc + num)
+		)
+		.subscribe(console.log); // La salida debe de ser 32
+})();
